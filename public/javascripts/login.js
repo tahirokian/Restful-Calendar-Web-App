c0b5009269e45dd074a9fe65fbcb00124fb6334a
Tahir Irshad <tahir.irshad@aalto.fi>
2016-08-29 11:43:38 +0300

    Updated and added comments

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,11 +1,12 @@
-/* login.js */
 $(document).ready(function() {
+  /* User clicked login button */
   $("#loginbtn").on("click", userLogin);
 });
 
 function userLogin(e) {
   e.preventDefault();
   var errorCnt = 0;
+  /* Check if user has filled in all fields? */
   $('#loginform input').each(function(index, val) {
     if($(this).val() === '') { errorCnt++; }
   });
@@ -19,16 +20,15 @@ function userLogin(e) {
       url: '/login',
       data: loginuser
     }).done(function(response, status) {
-      if(response.success){
-        window.location.replace(response.url);
-      } else{
-	alert(response.message);
+      if(response.success){   //If login sucess
+        window.location.replace('/home');
+      } else {
+        alert('Entered credentials are not correct.');
       }
     });
   }
   else {
-    alert('Please provide correct information for Login');
+    alert('Please provide username and password for login.');
     return false;
   }
 }
-
