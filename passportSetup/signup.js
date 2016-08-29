b7ec1035a4cd90aff790a6f6f61741a79ea7b35e
Tahir Irshad <tahir.irshad@aalto.fi>
2016-08-29 12:08:02 +0300

    Updated and added comments

--- passportSetup/signup.js
+++ passportSetup/signup.js
@@ -1,37 +1,35 @@
 var passportLocal = require('passport-local').Strategy;
-var User = require('../models/user');
 var bCrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
 
-// Generates hash using bCrypt
+/* Generates hash using bCrypt. */
 var createHash = function(password){
   return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
 }
 
 module.exports = function(passport){
   passport.use('signup', new passportLocal(
-    {passReqToCallback: true},    // allows to pass the entire request (req) to the callback
+    {passReqToCallback: true},    /* allows to pass the entire request (req) to the callback. */
     function(req, username, password, done) {
       User.findOne({ 'username' :  username }, function(err, user) {
+        /* If there is some error in signup. */
         if (err){
-          console.log('Error in SignUp: ' + err);
           return done(err);
         }
-        // already exists
+        /* If user already exists. */
         if (user) {
-          console.log('This username exists already.');
-          //return done(null, false, {message: 'Someone already has this username. Please try another.'});
           return done(null, false, req.flash('signupMessage', 'Username is already taken.'));
         } else {
-          // create the user, if there is no user present with that email.
+          /* Create the user in database. */
           var newUser = new User();
           newUser.username = username;
           newUser.password = createHash(password);
           newUser.email = req.body.email;
           newUser.fullname = req.body.fullname;
           newUser.save(function(err) {
+            /* If there is some error in saving user to database. */
             if (err){
-              console.log('Error while saving the user: ' + err);  
-              throw err;  
+              throw err;
             }
             return done(null, newUser);
           });
