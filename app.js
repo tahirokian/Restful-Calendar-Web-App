0e2e45a8fc4573312d39854bbb00ced187851a72
Tahir Irshad <tahir.irshad@aalto.fi>
2016-08-29 12:01:36 +0300

    Added comments

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//Connect to mongo db
+/* Connect to mongo db. */
 require('./db')();
 
 var app = express();
 
-// view engine setup
+/* View engine setup. */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+/* Uncomment after placing your favicon in /public */
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,33 +28,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// required for passport
+/* Required for passport. */
 app.use(session({
   secret: "secret",
   saveUninitialized: true,
   resave: true
 }));
 app.use(passport.initialize());
-app.use(passport.session()); 		// persistent login sessions
-app.use(flash()); 			// use connect-flash for flash messages stored in session
+/* Persistent login sessions. */
+app.use(passport.session());
+/* Use connect-flash for flash messages stored in session. */
+app.use(flash());
 
-//Passport configuration
+/* Passport configuration. */
 require('./passportSetup/initSession')(passport);
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/* Catch 404 and forward to error handler. */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+/* Error handlers. */
 
-// development error handler
-// will print stacktrace
+/* Development error handler will print stacktrace. */
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -65,8 +66,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+/* Production error handler no stacktraces leaked to user. */
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error.ejs', {
